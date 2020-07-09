77a376e05ac1a34d15c4c40dd7f24e451cb2f0ec
ivanmontero <ivanspmontero@gmail.com>
2020-07-08 18:51:55 -0700

    Update up to master

--- fairseq/modules/autoencoder.py
+++ fairseq/modules/autoencoder.py
@@ -1,8 +1,18 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Dict, List, Optional
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from fairseq import utils
 from fairseq.modules import LayerNorm, MultiheadAttention
+from fairseq.modules.quant_noise import quant_noise
+from fairseq.modules.fairseq_dropout import FairseqDropout
+from torch import Tensor
+
 
 class AutoencoderDecoderLayer(nn.Module):
     """Decoder layer block.
@@ -21,33 +31,39 @@ class AutoencoderDecoderLayer(nn.Module):
             (default: False).
     """
 
-    def __init__(self, args, no_encoder_attn=False, add_bias_kv=False, add_zero_attn=False):
+    def __init__(
+        self, args, no_encoder_attn=False, add_bias_kv=False, add_zero_attn=False
+    ):
         super().__init__()
         self.embed_dim = args.decoder_embed_dim
         self.autoencoder_hidden_size = getattr(args, 'autoencoder_hidden_size', self.embed_dim) # autoencoder_hidden_size
-        self.cross_self_attention = getattr(args, 'cross_self_attention', False)
-        self.self_attn = MultiheadAttention(
-            embed_dim=self.embed_dim,
-            num_heads=args.decoder_attention_heads,
-            dropout=args.attention_dropout,
+        self.dropout_module = FairseqDropout(args.dropout, module_name=self.__class__.__name__)
+        self.quant_noise = getattr(args, "quant_noise_pq", 0)
+        self.quant_noise_block_size = getattr(args, "quant_noise_pq_block_size", 8)
+
+        self.cross_self_attention = getattr(args, "cross_self_attention", False)
+
+        self.self_attn = self.build_self_attention(
+            self.embed_dim,
+            args,
             add_bias_kv=add_bias_kv,
             add_zero_attn=add_zero_attn,
-            self_attention=not self.cross_self_attention,
         )
-        self.dropout = args.dropout
         self.activation_fn = utils.get_activation_fn(
-            activation=getattr(args, 'activation_fn', 'relu')
+            activation=getattr(args, "activation_fn", "relu")
         )
-        self.activation_dropout = getattr(args, 'activation_dropout', 0)
-        if self.activation_dropout == 0:
+        activation_dropout_p = getattr(args, "activation_dropout", 0)
+        if activation_dropout_p == 0:
             # for backwards compatibility with models that use args.relu_dropout
-            self.activation_dropout = getattr(args, 'relu_dropout', 0)
+            activation_dropout_p = getattr(args, "relu_dropout", 0)
+        self.activation_dropout_module = FairseqDropout(
+            float(activation_dropout_p), module_name=self.__class__.__name__)
         self.normalize_before = args.decoder_normalize_before
 
         # use layerNorm rather than FusedLayerNorm for exporting.
         # char_inputs can be used to determint this.
         # TODO  remove this once we update apex with the fix
-        export = getattr(args, 'char_inputs', False)
+        export = getattr(args, "char_inputs", False)
         self.self_attn_layer_norm = LayerNorm(self.embed_dim, export=export)
 
         if no_encoder_attn:
@@ -59,14 +75,47 @@ class AutoencoderDecoderLayer(nn.Module):
             self.proj_gz = nn.Linear(self.autoencoder_hidden_size, self.embed_dim)
             self.encoder_attn_layer_norm = LayerNorm(self.embed_dim, export=export)
 
-        self.fc1 = Linear(self.embed_dim, args.decoder_ffn_embed_dim)
-        self.fc2 = Linear(args.decoder_ffn_embed_dim, self.embed_dim)
+        self.fc1 = self.build_fc1(
+            self.embed_dim, args.decoder_ffn_embed_dim, self.quant_noise, self.quant_noise_block_size
+        )
+        self.fc2 = self.build_fc2(
+            args.decoder_ffn_embed_dim, self.embed_dim, self.quant_noise, self.quant_noise_block_size
+        )
 
         self.final_layer_norm = LayerNorm(self.embed_dim, export=export)
         self.need_attn = True
 
         self.onnx_trace = False
 
+    def build_fc1(self, input_dim, output_dim, q_noise, qn_block_size):
+        return quant_noise(nn.Linear(input_dim, output_dim), q_noise, qn_block_size)
+
+    def build_fc2(self, input_dim, output_dim, q_noise, qn_block_size):
+        return quant_noise(nn.Linear(input_dim, output_dim), q_noise, qn_block_size)
+
+    def build_self_attention(self, embed_dim, args, add_bias_kv=False, add_zero_attn=False):
+        return MultiheadAttention(
+            embed_dim,
+            args.decoder_attention_heads,
+            dropout=args.attention_dropout,
+            add_bias_kv=add_bias_kv,
+            add_zero_attn=add_zero_attn,
+            self_attention=not getattr(args, "cross_self_attention", False),
+            q_noise=self.quant_noise,
+            qn_block_size=self.quant_noise_block_size,
+        )
+
+    def build_encoder_attention(self, embed_dim, args):
+        return MultiheadAttention(
+            embed_dim,
+            args.decoder_attention_heads,
+            kdim=getattr(args, "encoder_embed_dim", None),
+            vdim=getattr(args, "encoder_embed_dim", None),
+            dropout=args.attention_dropout,
+            encoder_decoder_attention=True,
+            q_noise=self.quant_noise,
+            qn_block_size=self.quant_noise_block_size,
+        )
 
     def prepare_for_onnx_export_(self):
         self.onnx_trace = True
@@ -74,14 +123,14 @@ class AutoencoderDecoderLayer(nn.Module):
     def forward(
         self,
         x,
-        bottleneck_out=None,
-        incremental_state=None,
-        prev_self_attn_state=None,
-        prev_attn_state=None,
-        self_attn_mask=None,
-        self_attn_padding_mask=None,
-        need_attn=False,
-        need_head_weights=False,
+        bottleneck_out: Optional[torch.Tensor] = None,
+        incremental_state: Optional[Dict[str, Dict[str, Optional[Tensor]]]] = None,
+        prev_self_attn_state: Optional[List[torch.Tensor]] = None,
+        prev_attn_state: Optional[List[torch.Tensor]] = None,
+        self_attn_mask: Optional[torch.Tensor] = None,
+        self_attn_padding_mask: Optional[torch.Tensor] = None,
+        need_attn: bool = False,
+        need_head_weights: bool = False,
     ):
         """
         Args:
@@ -100,26 +149,41 @@ class AutoencoderDecoderLayer(nn.Module):
             need_attn = True
 
         residual = x
-        x = self.maybe_layer_norm(self.self_attn_layer_norm, x, before=True)
+        if self.normalize_before:
+            x = self.self_attn_layer_norm(x)
         if prev_self_attn_state is not None:
-            if incremental_state is None:
-                incremental_state = {}
             prev_key, prev_value = prev_self_attn_state[:2]
-            saved_state = {"prev_key": prev_key, "prev_value": prev_value}
+            saved_state: Dict[str, Optional[Tensor]] = {
+                "prev_key": prev_key,
+                "prev_value": prev_value,
+            }
             if len(prev_self_attn_state) >= 3:
                 saved_state["prev_key_padding_mask"] = prev_self_attn_state[2]
+            assert incremental_state is not None
             self.self_attn._set_input_buffer(incremental_state, saved_state)
-
-        # if self.cross_self_attention and not (incremental_state is not None and "prev_key" in self.self_attn._get_input_buffer(incremental_state)):
+        # _self_attn_input_buffer = self.self_attn._get_input_buffer(incremental_state)
+        # if self.cross_self_attention and not (
+        #     incremental_state is not None
+        #     and _self_attn_input_buffer is not None
+        #     and "prev_key" in _self_attn_input_buffer
+        # ):
         #     if self_attn_mask is not None:
-        #         self_attn_mask = torch.cat((x.new(x.size(0), encoder_out.size(0)).zero_(), self_attn_mask), dim=1)
+        #         assert encoder_out is not None
+        #         self_attn_mask = torch.cat(
+        #             (x.new_zeros(x.size(0), encoder_out.size(0)), self_attn_mask), dim=1
+        #         )
         #     if self_attn_padding_mask is not None:
         #         if encoder_padding_mask is None:
-        #             encoder_padding_mask = self_attn_padding_mask.new(encoder_out.size(1), encoder_out.size(0)).zero_()
-        #         self_attn_padding_mask = torch.cat((encoder_padding_mask, self_attn_padding_mask), dim=1)
+        #             assert encoder_out is not None
+        #             encoder_padding_mask = self_attn_padding_mask.new_zeros(
+        #                 encoder_out.size(1), encoder_out.size(0)
+        #             )
+        #         self_attn_padding_mask = torch.cat(
+        #             (encoder_padding_mask, self_attn_padding_mask), dim=1
+        #         )
+        #     assert encoder_out is not None
         #     y = torch.cat((encoder_out, x), dim=0)
         # else:
-        #     y = x
         y = x
 
         x, attn = self.self_attn(
@@ -131,25 +195,27 @@ class AutoencoderDecoderLayer(nn.Module):
             need_weights=False,
             attn_mask=self_attn_mask,
         )
-        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.dropout_module(x)
         x = residual + x
-        x = self.maybe_layer_norm(self.self_attn_layer_norm, x, after=True)
+        if not self.normalize_before:
+            x = self.self_attn_layer_norm(x)
 
         if self.encoder_attn_layer_norm is not None:
             residual = x
-            x = self.maybe_layer_norm(self.encoder_attn_layer_norm, x, before=True)
-
-
+            if self.normalize_before:
+                x = self.encoder_attn_layer_norm(x)
+        
             bottleneck_expanded = bottleneck_out.expand_as(x)
             x = torch.sigmoid(self.proj_gh(x) + self.proj_gz(bottleneck_expanded)) * self.proj_v(bottleneck_expanded)
-            #         return (output,)
             # if prev_attn_state is not None:
-            #     if incremental_state is None:
-            #         incremental_state = {}
             #     prev_key, prev_value = prev_attn_state[:2]
-            #     saved_state = {"prev_key": prev_key, "prev_value": prev_value}
+            #     saved_state: Dict[str, Optional[Tensor]] = {
+            #         "prev_key": prev_key,
+            #         "prev_value": prev_value,
+            #     }
             #     if len(prev_attn_state) >= 3:
             #         saved_state["prev_key_padding_mask"] = prev_attn_state[2]
+            #     assert incremental_state is not None
             #     self.encoder_attn._set_input_buffer(incremental_state, saved_state)
 
             # x, attn = self.encoder_attn(
@@ -162,65 +228,43 @@ class AutoencoderDecoderLayer(nn.Module):
             #     need_weights=need_attn or (not self.training and self.need_attn),
             #     need_head_weights=need_head_weights,
             # )
-
-            x = F.dropout(x, p=self.dropout, training=self.training)
+            x = self.dropout_module(x)
             x = residual + x
-            x = self.maybe_layer_norm(self.encoder_attn_layer_norm, x, after=True)
+            if not self.normalize_before:
+                x = self.encoder_attn_layer_norm(x)
 
         residual = x
-        x = self.maybe_layer_norm(self.final_layer_norm, x, before=True)
+        if self.normalize_before:
+            x = self.final_layer_norm(x)
+
         x = self.activation_fn(self.fc1(x))
-        x = F.dropout(x, p=self.activation_dropout, training=self.training)
+        x = self.activation_dropout_module(x)
         x = self.fc2(x)
-        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.dropout_module(x)
         x = residual + x
-        x = self.maybe_layer_norm(self.final_layer_norm, x, after=True)
+        if not self.normalize_before:
+            x = self.final_layer_norm(x)
         if self.onnx_trace and incremental_state is not None:
             saved_state = self.self_attn._get_input_buffer(incremental_state)
+            assert saved_state is not None
             if self_attn_padding_mask is not None:
-                self_attn_state = saved_state["prev_key"], saved_state["prev_value"], saved_state["prev_key_padding_mask"]
+                self_attn_state = [
+                    saved_state["prev_key"],
+                    saved_state["prev_value"],
+                    saved_state["prev_key_padding_mask"],
+                ]
             else:
-                self_attn_state = saved_state["prev_key"], saved_state["prev_value"]
+                self_attn_state = [saved_state["prev_key"], saved_state["prev_value"]]
             return x, attn, self_attn_state
-        return x, attn
+        return x, attn, None
 
-    def maybe_layer_norm(self, layer_norm, x, before=False, after=False):
-        assert before ^ after
-        if after ^ self.normalize_before:
-            return layer_norm(x)
-        else:
-            return x
-
-    def make_generation_fast_(self, need_attn=False, **kwargs):
+    def make_generation_fast_(self, need_attn: bool = False, **kwargs):
         self.need_attn = need_attn
 
-# class HFBertAECrossAttention(nn.Module):
-#     def __init__(self, config, autoencoder_hidden_size):
-#         super().__init__()
-#         self.autoencoder_hidden_size = autoencoder_hidden_size
-#         self.proj_v = nn.Linear(self.autoencoder_hidden_size, config.hidden_size)
-#         self.proj_gh = nn.Linear(config.hidden_size, config.hidden_size)
-#         self.proj_gz = nn.Linear(self.autoencoder_hidden_size, config.hidden_size)
-
-#     def prune_heads(self, heads):
-#         pass
-
-#     # Override here
-#     def forward(
-#         self,
-#         hidden_states,
-#         attention_mask=None,
-#         head_mask=None,
-#         encoder_hidden_states=None,
-#         encoder_attention_mask=None,
-#     ):
-#         z_expand = encoder_hidden_states.expand_as(hidden_states)
-#         output = torch.sigmoid(self.proj_gh(hidden_states) + self.proj_gz(z_expand)) * self.proj_v(z_expand)
-#         return (output,)
 
 def Linear(in_features, out_features, bias=True):
     m = nn.Linear(in_features, out_features, bias)
     nn.init.xavier_uniform_(m.weight)
     if bias:
-        nn.init.constant_(m.bias, 0.)
+        nn.init.constant_(m.bias, 0.0)
     return m
