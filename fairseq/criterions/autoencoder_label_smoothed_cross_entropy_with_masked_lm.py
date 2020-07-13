3b4705a17b99e8e032fb91ca38ba1594c9f9a419
ivanmontero <ivanspmontero@gmail.com>
2020-07-12 22:25:48 -0700

    Change scale

--- fairseq/criterions/autoencoder_label_smoothed_cross_entropy_with_masked_lm.py
+++ fairseq/criterions/autoencoder_label_smoothed_cross_entropy_with_masked_lm.py
@@ -105,7 +105,7 @@ class AutoencoderLabelSmoothedCrossEntropyWithMaskedLmCriterion(FairseqCriterion
 
         metrics.log_scalar('loss', loss_sum / sample_size / math.log(2), sample_size, round=3)
         metrics.log_scalar('nll_loss', nll_loss_sum / ntokens / math.log(2), ntokens, round=3)
-        metrics.log_scalar('masked_loss', masked_loss_sum / ntokens / math.log(2), ntokens, round=3)
+        metrics.log_scalar('masked_loss', masked_loss_sum / sample_size / math.log(2), sample_size, round=3)
         metrics.log_derived('ppl', lambda meters: utils.get_perplexity(meters['nll_loss'].avg))
 
     @staticmethod
